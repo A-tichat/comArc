aea8f94391fb3edb4080955d2b95af60297302ca
A-tichat <gamecup84@gmail.com>
2019-11-04 20:05:11 +0700

    update all type except jalr

--- src/app/App.java
+++ src/app/App.java
@@ -6,34 +6,59 @@ import java.util.Scanner;
 
 public class App {
 
-    public static String funcBinary(int dec, int l){ // 5, 3
-        String bi = "";
-        int i=0;
-        if (dec >= 0){
-            while (i<l){
-                if (dec == 0){
-                    bi = "0"+bi;
-                    dec = dec/2; 
-                }else if (dec == 1){
-                    bi = "1"+bi;
-                    dec = dec/2;
-                }else{
-                    bi = Integer.toString(dec%2)+bi;
-                    dec = dec/2;
-                }
-                i++;
-            }
-        }else {
+    public static String funcTwoCom(StringBuffer str) {
+        int n = str.length();
 
+        int i;
+        for (i = n - 1; i >= 0; i--)
+            if (str.charAt(i) == '1')
+                break;
+
+        if (i == -1)
+            return "1" + str;
+
+        for (int k = i - 1; k >= 0; k--) {
+            if (str.charAt(k) == '1')
+                str.replace(k, k + 1, "0");
+            else
+                str.replace(k, k + 1, "1");
         }
-        
+
+        return str.toString();
+    }
+
+    public static String funcBinary(int dec, int l) { // 5, 3
+        String bi = "";
+        int i = 0;
+        boolean flag = false;
+        if (dec < 0) {
+            dec = dec * (0 - 1);
+            flag = true;
+        }
+        while (i < l) {
+            if (dec == 0) {
+                bi = "0" + bi;
+                dec = dec / 2;
+            } else if (dec == 1) {
+                bi = "1" + bi;
+                dec = dec / 2;
+            } else {
+                bi = Integer.toString(dec % 2) + bi;
+                dec = dec / 2;
+            }
+            i++;
+        }
+        if (flag) {
+            bi = funcTwoCom(new StringBuffer(bi));
+        }
+
         return bi;
     }
 
     public static void main(String[] args) {
         String[] data = new String[100]; // read max 100 line
         int addr = 0;// addr of line
-        Map<String, Integer> label = new HashMap<String, Integer>(); //store label of each line
+        Map<String, Integer> label = new HashMap<String, Integer>(); // store label of each line
 
         // read code form file
         try {
@@ -52,7 +77,7 @@ public class App {
         }
 
         int r = 0;
-        while (data[r] != null){
+        while (data[r] != null) {
             String[] arrOfdatat = data[r].split("	");
             if (arrOfdatat[1].equals(".fill")) {
                 if (arrOfdatat[2].matches("-?(0|[1-9]\\d*)")) {
@@ -60,7 +85,7 @@ public class App {
                 } else {
                     label.put(arrOfdatat[0], label.get(arrOfdatat[2]));
                 }
-            }else if (arrOfdatat[0] != "") {
+            } else if (!arrOfdatat[0].equals("")) {
                 label.put(arrOfdatat[0], r); // set label to addr
                 // label.get(arrOfdata[]) to get addr
             }
@@ -69,50 +94,78 @@ public class App {
 
         addr = 0;
         while (data[addr] != null) {
-            String[] arrOfdata = data[addr].split("	"); //feild of instruction
-            String BinaryCode = "0000000"; //machinecode binary bit
+            String[] arrOfdata = data[addr].split("	"); // feild of instruction
+            String BinaryCode = "0000000"; // machinecode binary bit
             int macCode = 0; // machinecode decimal bit
-            
 
             if (arrOfdata[1].equals("add")) {
                 BinaryCode += "000";
                 // TO DO SOMETHING
-                
-                //macCode = Integer.parseInt(BinaryCode,2);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[2]), 3);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[3]), 3);
+                for (int indexz = 3; indexz <= 15; indexz++)
+                    BinaryCode = BinaryCode + "0";
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[4]), 3);
+                macCode = Integer.parseInt(BinaryCode, 2);
             } else if (arrOfdata[1].equals("nand")) {
                 BinaryCode += "001";
                 // TO DO SOMETHING
-                //macCode = Integer.parseInt(BinaryCode,2);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[2]), 3);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[3]), 3);
+                BinaryCode += funcBinary(0, 13);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[4]), 3);
+                macCode = Integer.parseInt(BinaryCode, 2);
             } else if (arrOfdata[1].equals("lw")) {
                 BinaryCode += "010";
                 // TO DO SOMETHING
-                
-                //macCode = Integer.parseInt(BinaryCode,2);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[2]), 3);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[3]), 3);
+                if (arrOfdata[4].matches("-?(0|[1-9]\\d*)")) {
+                    BinaryCode += funcBinary(Integer.parseInt(arrOfdata[4]), 16);
+                } else {
+                    BinaryCode += funcBinary(label.get(arrOfdata[4]), 16);
+                }
+                macCode = Integer.parseInt(BinaryCode, 2);
             } else if (arrOfdata[1].equals("sw")) {
                 BinaryCode += "011";
                 // TO DO SOMETHING
-                //macCode = Integer.parseInt(BinaryCode,2);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[2]), 3);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[3]), 3);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[4]), 16);
+                macCode = Integer.parseInt(BinaryCode, 2);
             } else if (arrOfdata[1].equals("beq")) {
                 BinaryCode += "100";
                 // TO DO SOMETHING
-                //macCode = Integer.parseInt(BinaryCode,2);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[2]), 3);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[3]), 3);
+                int dst = 0;
+                if (arrOfdata[4].matches("-?(0|[1-9]\\d*)")) {
+                    dst = Integer.parseInt(arrOfdata[4]);
+                } else {
+                    dst = label.get(arrOfdata[4]);
+                    if (dst < addr) {
+                        dst = dst * (0 - 1);
+                    }
+                }
+                BinaryCode += funcBinary(dst, 16);
+                macCode = Integer.parseInt(BinaryCode, 2);
             } else if (arrOfdata[1].equals("jalr")) {
                 BinaryCode += "101";
                 // TO DO SOMETHING
 
-                //macCode = Integer.parseInt(BinaryCode,2);
+                // macCode = Integer.parseInt(BinaryCode,2);
             } else if (arrOfdata[1].equals("halt")) {
                 BinaryCode += "110";
                 // TO DO SOMETHING
                 for (int indexz = 0; indexz <= 21; indexz++)
                     BinaryCode = BinaryCode + "0";
-                macCode = Integer.parseInt(BinaryCode,2); 
+                macCode = Integer.parseInt(BinaryCode, 2);
             } else if (arrOfdata[1].equals("noop")) {
                 BinaryCode += "111";
                 // TO DO SOMETHING
                 for (int indexz = 0; indexz <= 21; indexz++)
                     BinaryCode = BinaryCode + "0";
-                macCode = Integer.parseInt(BinaryCode,2);  
+                macCode = Integer.parseInt(BinaryCode, 2);
 
             } else if (arrOfdata[1].equals(".fill")) {
                 if (arrOfdata[2].matches("-?(0|[1-9]\\d*)")) {
@@ -132,8 +185,6 @@ public class App {
             System.out.println(macCode);
             addr++;
         }
-        System.out.println(label);
-        System.out.println(funcBinary(55, 4));
     }
 
 }
