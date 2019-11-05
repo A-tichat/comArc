12996cf32387a23ecc4b549da2f361304450020e
A-tichat <gamecup84@gmail.com>
2019-11-05 15:41:26 +0700

    l1

--- src/app/App.java
+++ src/app/App.java
@@ -152,7 +152,11 @@ public class App {
             } else if (arrOfdata[1].equals("jalr")) {
                 BinaryCode += "101";
                 // TO DO SOMETHING
-                //macCode = Integer.parseInt(BinaryCode,2);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[2]), 3);
+                BinaryCode += funcBinary(Integer.parseInt(arrOfdata[3]), 3);
+                for(int indexz =0; indexz<=15;indexz++)
+                    BinaryCode = BinaryCode + "0";
+                macCode = Integer.parseInt(BinaryCode,2);
             } else if (arrOfdata[1].equals("halt")) {
                 BinaryCode += "110";
                 // TO DO SOMETHING
